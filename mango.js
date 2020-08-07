9770f61cfab2445afeb93a38d50ffd35f2db2f21
tanusri123 <67907113+tanusri123@users.noreply.github.com>
2020-08-07 19:03:18 +0530

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,20 @@
+var options ={
+    isStatic=false,
+    resistution=0,
+    friction=1,
+}
+var mango1=new mango(410,200);
+var mango2=new mango(450,250);
+var mango3=new mango(500,280);
+var mango4=new mango(550,190);
+var mango5=new mango(410,200);
+
+display();{
+mango1=display();
+mango2=display();
+mango3=display();
+mango4=display();
+mango5=display();
+}
+
+
---
+++ stone.js
@@ -0,0 +1,6 @@
+var options={
+isStatic=false,
+resistution=1,
+friction=1,
+density=1.2,
+}
